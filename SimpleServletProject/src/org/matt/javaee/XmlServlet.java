b0dd1dcb38ef3f5cd624d7e318567adb1a2cbad5
mattcarp12 <mattcarp88@gmail.com>
2019-08-17 13:29:00 -0400

    update

--- SimpleServletProject/src/org/matt/javaee/XmlServlet.java
+++ SimpleServletProject/src/org/matt/javaee/XmlServlet.java
@@ -4,6 +4,7 @@
 package org.matt.javaee;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,9 +19,24 @@ public class XmlServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().print("<html><h1>Hello World from XmlServletPath!!!</h1></html>");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 		System.out.println("Hello from GET method!");
+		String username = request.getParameter("userName");
+		out.println(username + " says hello!");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		System.out.println("Hello from POST method!");
+		String username = request.getParameter("userName");
+		String fullname = request.getParameter("fullName");
+		String prof = request.getParameter("prof");
+		out.println(username + " says hello!");
+		out.println("We know your full name is: " + fullname);
+		out.println("You are a " + prof);
 	}
 
 }
